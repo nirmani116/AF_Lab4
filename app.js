cae674043d7a32f19e04bbe0dab9bfdb1ec8c132
Nirmani <awnirmani16@gmail.com>
2026-02-23 00:26:50 +0530

    Async-Await

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const condition = true;
 
-const myPromise = new Promise((resolve, reject) => { 
-  if (condition) { 
-    resolve('Success!'); 
-  } else { 
-    reject('Failure!'); 
-  } 
-}); 
- 
-myPromise.then((result) => { 
-  console.log(result); 
-}).catch((error) => { 
-  console.log(error); 
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve("Success!");
+  } else {
+    reject("Failure!");
+  }
 });
+
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+myFunction();
