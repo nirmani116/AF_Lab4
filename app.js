cd88c54dec4bddb9694fadc8c56e85e4a7892dee
Nirmani <awnirmani16@gmail.com>
2026-02-22 17:20:02 +0530

    Created a web server

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const fs = require('fs'); 
-fs.writeFile('file.txt', 'Hello World!', function (err) { 
-  if (err) throw err; 
-  console.log('File saved!'); 
-}); 
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(3000, () => {
+  console.log("Server running at http://localhost:3000/");
+});
